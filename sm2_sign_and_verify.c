ed4704b362126829c40d23d39bccd95d1fab4c9b
greendow <china_hanwei@hotmail.com>
2018-11-20 23:03:59 +0800

    Update sm2_sign_and_verify.c

--- sm2_sign_and_verify.c
+++ sm2_sign_and_verify.c
@@ -19,12 +19,12 @@
 
 /*********************************************************/
 int sm2_sign_data_test(const unsigned char *message,
-	                   const int message_len,
-					   const unsigned char *id,
-					   const int id_len,
-					   const unsigned char *pub_key,
-					   const unsigned char *pri_key,
-					   SM2_SIGNATURE_STRUCT *sm2_sig)
+	               const int message_len,
+		       const unsigned char *id,
+		       const int id_len,
+		       const unsigned char *pub_key,
+		       const unsigned char *pri_key,
+		       SM2_SIGNATURE_STRUCT *sm2_sig)
 {
 	int error_code;
 	unsigned char digest[32];
@@ -43,12 +43,12 @@ int sm2_sign_data_test(const unsigned char *message,
 
 	if ( error_code = sm3_digest_with_preprocess(message,
 	                                             message_len,
-												 id,
-												 id_len,
-												 pub_key,
-												 digest) )
+						     id,
+						     id_len,
+						     pub_key,
+						     digest) )
 	{
-		return error_code;
+	   return error_code;
 	}
 
 	error_code = ALLOCATION_MEMORY_FAIL;
@@ -69,129 +69,129 @@ int sm2_sign_data_test(const unsigned char *message,
 	bn_dif = BN_CTX_get(ctx);
 	if ( !(bn_dif) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( !(group = EC_GROUP_new_by_curve_name(NID_sm2)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 
 	if ( !(k_G = EC_POINT_new(group)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 
 	error_code = COMPUTE_SM2_SIGNATURE_FAIL;
 	if ( !(BN_one(bn_one)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 
 	if ( !(BN_bin2bn(pri_key, 32, bn_d)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 
 	if ( !(BN_bin2bn(digest, sizeof(digest), bn_e)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( !(bn_order = EC_GROUP_get0_order(group)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( !(generator = EC_GROUP_get0_generator(group)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	
 	do
 	{
 		if ( !(BN_bin2bn(k, sizeof(k), bn_k)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( BN_is_zero(bn_k) )
 		{
-			continue;
+		   continue;
 		}
 		if ( !(EC_POINT_mul(group, k_G, bn_k, NULL, NULL, ctx)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( !(EC_POINT_get_affine_coordinates_GFp(group,
 		                                           k_G,
-												   bn_x,
-												   bn_tmp,
-												   ctx)) )
+							   bn_x,
+							   bn_tmp,
+							   ctx)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( !(BN_mod_add(bn_r, bn_e, bn_x, bn_order, ctx)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
-		if ( BN_is_zero(bn_r) ) /* check if r==0 ? */
+		if ( BN_is_zero(bn_r) ) /* check if r == 0 ? */
 		{
-			continue;
+		   continue;
 		}
 		if ( !(BN_add(bn_tmp, bn_r, bn_k)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( !(BN_cmp(bn_tmp, bn_order)) )  /* check if (r + k) == n ? */
 		{
-			continue;
+		   continue;
 		}
 		if ( !(BN_add(bn_tmp, bn_one, bn_d)) )  /* compute (1 + d) */
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( !(BN_mod_inverse(bn_sum_inv, bn_tmp, bn_order, ctx)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( !(BN_mul(bn_tmp, bn_r, bn_d, ctx)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( !(BN_mod_sub(bn_dif, bn_k, bn_tmp, bn_order, ctx)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( !(BN_mod_mul(bn_s, bn_sum_inv, bn_dif, bn_order, ctx)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 	} while ( BN_is_zero(bn_s) );  /* check if s == 0 ? */
 	
 	if ( BN_bn2binpad(bn_r,
 	                  sm2_sig->r_coordinate,
-					  sizeof(sm2_sig->r_coordinate)) != sizeof(sm2_sig->r_coordinate) )
+			  sizeof(sm2_sig->r_coordinate)) != sizeof(sm2_sig->r_coordinate) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( BN_bn2binpad(bn_s,
 	                  sm2_sig->s_coordinate,
-					  sizeof(sm2_sig->s_coordinate)) != sizeof(sm2_sig->s_coordinate) )
+			  sizeof(sm2_sig->s_coordinate)) != sizeof(sm2_sig->s_coordinate) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	error_code = 0;
 
 clean_up:
     if (ctx)
 	{
-		BN_CTX_end(ctx);
-		BN_CTX_free(ctx);
+	   BN_CTX_end(ctx);
+	   BN_CTX_free(ctx);
 	}
 	if (group)
 	{
-		EC_GROUP_free(group);
+	   EC_GROUP_free(group);
 	}
 	if (k_G)
 	{
-		EC_POINT_free(k_G);
+	   EC_POINT_free(k_G);
 	}
 
 	return error_code;
@@ -200,11 +200,11 @@ clean_up:
 /*********************************************************/
 int sm2_sign_data(const unsigned char *message,
                   const int message_len,
-				  const unsigned char *id,
-				  const int id_len,
-				  const unsigned char *pub_key,
-				  const unsigned char *pri_key,
-				  SM2_SIGNATURE_STRUCT *sm2_sig)
+		  const unsigned char *id,
+		  const int id_len,
+		  const unsigned char *pub_key,
+		  const unsigned char *pri_key,
+		  SM2_SIGNATURE_STRUCT *sm2_sig)
 {
 	int error_code;
 	unsigned char digest[32];
@@ -219,12 +219,12 @@ int sm2_sign_data(const unsigned char *message,
 
 	if ( error_code = sm3_digest_with_preprocess(message,
 	                                             message_len,
-												 id,
-												 id_len,
-												 pub_key,
-												 digest) )
+						     id,
+						     id_len,
+						     pub_key,
+						     digest) )
 	{
-		return error_code;
+	   return error_code;
 	}
 
 	error_code = ALLOCATION_MEMORY_FAIL;
@@ -245,75 +245,75 @@ int sm2_sign_data(const unsigned char *message,
 	bn_dif = BN_CTX_get(ctx);
 	if ( !(bn_dif) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( !(group = EC_GROUP_new_by_curve_name(NID_sm2)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 
 	if ( !(k_G = EC_POINT_new(group)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 
 	error_code = COMPUTE_SM2_SIGNATURE_FAIL;
 	if ( !(BN_one(bn_one)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 
 	if ( !(BN_bin2bn(pri_key, 32, bn_d)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 
 	if ( !(BN_bin2bn(digest, sizeof(digest), bn_e)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( !(bn_order = EC_GROUP_get0_order(group)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( !(generator = EC_GROUP_get0_generator(group)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	
 	do
 	{
 		if ( !(BN_rand_range(bn_k, bn_order)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( BN_is_zero(bn_k) )
 		{
-			continue;
+		   continue;
 		}
 		if ( !(EC_POINT_mul(group, k_G, bn_k, NULL, NULL, ctx)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( !(EC_POINT_get_affine_coordinates_GFp(group,
 		                                           k_G,
-												   bn_x,
-												   bn_tmp,
-												   ctx)) )
+							   bn_x,
+							   bn_tmp,
+							   ctx)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( !(BN_mod_add(bn_r, bn_e, bn_x, bn_order, ctx)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( BN_is_zero(bn_r) ) /* check if r==0 ? */
 		{
-			continue;
+		   continue;
 		}
 		if ( !(BN_add(bn_tmp, bn_r, bn_k)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( !(BN_cmp(bn_tmp, bn_order)) )  /* check if (r + k) == n ? */
 		{
@@ -321,53 +321,53 @@ int sm2_sign_data(const unsigned char *message,
 		}
 		if ( !(BN_add(bn_tmp, bn_one, bn_d)) )  /* compute (1 + d) */
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( !(BN_mod_inverse(bn_sum_inv, bn_tmp, bn_order, ctx)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( !(BN_mul(bn_tmp, bn_r, bn_d, ctx)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( !(BN_mod_sub(bn_dif, bn_k, bn_tmp, bn_order, ctx)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 		if ( !(BN_mod_mul(bn_s, bn_sum_inv, bn_dif, bn_order, ctx)) )
 		{
-			goto clean_up;
+		   goto clean_up;
 		}
 	} while ( BN_is_zero(bn_s) );  /* check if s == 0 ? */
 	
 	if ( BN_bn2binpad(bn_r,
 	                  sm2_sig->r_coordinate,
-					  sizeof(sm2_sig->r_coordinate)) != sizeof(sm2_sig->r_coordinate) )
+			  sizeof(sm2_sig->r_coordinate)) != sizeof(sm2_sig->r_coordinate) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( BN_bn2binpad(bn_s,
 	                  sm2_sig->s_coordinate,
-					  sizeof(sm2_sig->s_coordinate)) != sizeof(sm2_sig->s_coordinate) )
+			  sizeof(sm2_sig->s_coordinate)) != sizeof(sm2_sig->s_coordinate) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	error_code = 0;
 
 clean_up:
     if (ctx)
 	{
-		BN_CTX_end(ctx);
-		BN_CTX_free(ctx);
+	   BN_CTX_end(ctx);
+	   BN_CTX_free(ctx);
 	}
 	if (group)
 	{
-		EC_GROUP_free(group);
+	   EC_GROUP_free(group);
 	}
 	if (k_G)
 	{
-		EC_POINT_free(k_G);
+	   EC_POINT_free(k_G);
 	}
 
 	return error_code;
@@ -376,10 +376,10 @@ clean_up:
 /*********************************************************/
 int sm2_verify_sig(const unsigned char *message,
                    const int message_len,
-				   const unsigned char *id,
-				   const int id_len,
-				   const unsigned char *pub_key,
-				   SM2_SIGNATURE_STRUCT *sm2_sig)
+		   const unsigned char *id,
+		   const int id_len,
+		   const unsigned char *pub_key,
+		   SM2_SIGNATURE_STRUCT *sm2_sig)
 {
 	int error_code;
 	unsigned char digest[32];
@@ -395,12 +395,12 @@ int sm2_verify_sig(const unsigned char *message,
 
 	if ( error_code = sm3_digest_with_preprocess(message,
 	                                             message_len,
-												 id,
-												 id_len,
-												 pub_key,
-												 digest) )
+						     id,
+						     id_len,
+						     pub_key,
+						     digest) )
 	{
-		return error_code;
+	   return error_code;
 	}
 
 	memcpy(pub_key_x, (pub_key + 1), sizeof(pub_key_x));
@@ -423,141 +423,141 @@ int sm2_verify_sig(const unsigned char *message,
 	bn_R = BN_CTX_get(ctx);
 	if ( !(bn_R) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( !(group = EC_GROUP_new_by_curve_name(NID_sm2)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	
 	if ( !(ec_pub_key_pt = EC_POINT_new(group)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( !(ec_pt1 = EC_POINT_new(group)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( !(ec_pt2 = EC_POINT_new(group)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 
 	error_code = VERIFY_SM2_SIGNATURE_FAIL;
 	if ( !(BN_bin2bn(digest, sizeof(digest), bn_e)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( !(BN_bin2bn(sm2_sig->r_coordinate, sizeof(sm2_sig->r_coordinate), bn_r)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( !(BN_bin2bn(sm2_sig->s_coordinate, sizeof(sm2_sig->s_coordinate), bn_s)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( !(BN_bin2bn(pub_key_x, sizeof(pub_key_x), bn_pub_key_x)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( !(BN_bin2bn(pub_key_y, sizeof(pub_key_y), bn_pub_key_y)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 
 	if ( !(bn_order = EC_GROUP_get0_order(group)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( !(generator = EC_GROUP_get0_generator(group)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 
 	if ( (BN_is_zero(bn_r)) || (BN_cmp(bn_r, bn_order) != (-1)) )
 	{
-		error_code = INVALID_SM2_SIGNATURE;
-		goto clean_up;
+	   error_code = INVALID_SM2_SIGNATURE;
+	   goto clean_up;
 	}
 	if ( (BN_is_zero(bn_s)) || (BN_cmp(bn_s, bn_order) != (-1)) )
 	{
-		error_code = INVALID_SM2_SIGNATURE;
-		goto clean_up;
+	   error_code = INVALID_SM2_SIGNATURE;
+	   goto clean_up;
 	}
 
 	if ( !(BN_mod_add(bn_t, bn_r, bn_s, bn_order, ctx)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( BN_is_zero(bn_t) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	
 	if ( !(EC_POINT_mul(group, ec_pt1, bn_s, NULL, NULL, ctx)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	
 	if ( !(EC_POINT_set_affine_coordinates_GFp(group,
 	                                           ec_pub_key_pt,
-											   bn_pub_key_x,
-											   bn_pub_key_y,
-											   ctx)) )
+						   bn_pub_key_x,
+						   bn_pub_key_y,
+						   ctx)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	
 	if ( !(EC_POINT_mul(group, ec_pt2, NULL, ec_pub_key_pt, bn_t, ctx)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	
 	if ( !(EC_POINT_add(group, ec_pt1, ec_pt1, ec_pt2, ctx)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	
 	if ( !(EC_POINT_get_affine_coordinates_GFp(group,
 	                                           ec_pt1,
-											   bn_x,
-											   bn_y,
-											   ctx)) )
+						   bn_x,
+						   bn_y,
+						   ctx)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	if ( !(BN_mod_add(bn_R, bn_e, bn_x, bn_order, ctx)) )
 	{
-		goto clean_up;
+	   goto clean_up;
 	}
 	
 	if ( !(BN_cmp(bn_r, bn_R)) ) /* verify signature succeed */
 	{
-		error_code = 0;
+	   error_code = 0;
 	}
 
 clean_up:
     if (ctx)
 	{
-		BN_CTX_end(ctx);
-		BN_CTX_free(ctx);
+	   BN_CTX_end(ctx);
+	   BN_CTX_free(ctx);
 	}
 	if (group)
 	{
-		EC_GROUP_free(group);
+	   EC_GROUP_free(group);
 	}
 
 	if (ec_pub_key_pt)
 	{
-		EC_POINT_free(ec_pub_key_pt);
+	   EC_POINT_free(ec_pub_key_pt);
 	}
 	if (ec_pt1)
 	{
-		EC_POINT_free(ec_pt1);
+	   EC_POINT_free(ec_pt1);
 	}
 	if (ec_pt2)
 	{
-		EC_POINT_free(ec_pt2);
+	   EC_POINT_free(ec_pt2);
 	}
 
 	return error_code;
